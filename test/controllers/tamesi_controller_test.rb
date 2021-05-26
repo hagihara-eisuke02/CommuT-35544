7664a3b5a8de127386938a8da9822fa114e2856e
hagihara-eisuke02 <e.hagihara.0430@gmail.com>
2021-05-26 18:13:38 +0900

    DB設計(ER図)・Rubocop追加

--- test/controllers/tamesi_controller_test.rb
+++ test/controllers/tamesi_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class TamesiControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get tamesi_index_url
     assert_response :success
   end
-
 end
