2c13c4783834911f2bba676dde2f2d4badbcb1e0
hagihara-eisuke02 <e.hagihara.0430@gmail.com>
2021-06-17 23:32:28 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ require('../mouseover')
 require('../slide')
 require('../page')
 require('./room')
-require('../chat')
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
